0aae0b94cbf7b2d60d9edede0b35cd9c62373883
fika54 <158376545+fika54@users.noreply.github.com>
2025-06-21 11:10:30 +0100

    Update dontleak.py

--- dontleak.py
+++ dontleak.py
@@ -1,6 +1,6 @@
-client_id = '63fh4ejnxd3ugj979tar2leqb6lgv5'
-client_secret = '15b6p2sbkd4f9fajg77udvfrkp15rd'
+client_id = ''
+client_secret = ''
 
-obs_server_ip = '10.110.136.61'
-obs_server_port = '4455'
-obs_server_password = '9iZbEOCAp23De3sy'
+obs_server_ip = ''
+obs_server_port = ''
+obs_server_password = ''
